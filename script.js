4475b2fe9627c769d25393bef14dedb0911ba635
alexander.kononov <alexander.kononov@aboutyou.com>
2022-02-13 14:32:33 +0100

    first commit

--- script.js
+++ script.js
@@ -6,33 +6,33 @@ const parseHeartRateValues = (data) => {
     // В Chrome 50+ используется DataView.
     const value = data.buffer ? data : new DataView(data);
     const flags = value.getUint8(0);
-  
+
     // Определяем формат
     const rate16Bits = flags & 0x1;
     const result = {};
     let index = 1;
-  
+
     // Читаем в зависимости от типа
     if (rate16Bits) {
-      result.heartRate = value.getUint16(index, /* littleEndian= */true);
-      index += 2;
+        result.heartRate = value.getUint16(index, /* littleEndian= */ true);
+        index += 2;
     } else {
-      result.heartRate = value.getUint8(index);
-      index += 1;
+        result.heartRate = value.getUint8(index);
+        index += 1;
     }
-  
+
     // RR интервалы
     const rrIntervalPresent = flags & 0x10;
     if (rrIntervalPresent) {
-      const rrIntervals = [];
-      for (; index + 1 < value.byteLength; index += 2) {
-        rrIntervals.push(value.getUint16(index, /* littleEndian= */true));
-      }
-      result.rrIntervals = rrIntervals;
+        const rrIntervals = [];
+        for (; index + 1 < value.byteLength; index += 2) {
+            rrIntervals.push(value.getUint16(index, /* littleEndian= */ true));
+        }
+        result.rrIntervals = rrIntervals;
     }
-  
+
     return result;
-  };
+};
 
 function initApp() {
     const btn = document.querySelector("[start-button]");
@@ -61,7 +61,7 @@ function handleCharacteristicValueChanged(e) {
     const value = parseHeartRateValues(e.target.value).rrIntervals;
     rawData.push(value);
     log(value);
-    
+
 }
 
 function pushData(index, value) {
